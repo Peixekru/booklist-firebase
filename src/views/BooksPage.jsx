fa66a238cbb3f51bc8a81b0b573cf07d1c152465
tiagodiniz <tiago.diniz@me.com>
2024-03-01 17:56:42 -0300

    Title Adjust

--- src/views/BooksPage.jsx
+++ src/views/BooksPage.jsx
@@ -10,7 +10,7 @@ function BooksPage() {
 
   const dispatch = useDispatch();
 	const books = useSelector(selectBooks).books;
-	const pageTitle = "📖 Book List with Router, Redux & Firebase";
+	const pageTitle = "📖 Book List with Router, Redux & Firebase v2";
 
   const bookStatus = useSelector(selectBooks).status;
 
